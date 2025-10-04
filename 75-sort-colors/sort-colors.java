546caf988b55d0d42620b2679310a2d1e29a43cb
yuktha_ms <166839897+yukthams@users.noreply.github.com>
2025-10-04 09:38:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (83.56%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,21 +1,28 @@
 class Solution {
     public void sortColors(int[] arr) {
-        //Arrays.sort(nums); - Brute force
-        int n=arr.length;
-        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+        int low = 0, mid = 0, high = arr.length - 1; // 3 pointers
 
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == 0) cnt0++;
-            else if (arr[i] == 1) cnt1++;
-            else cnt2++;
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                // swapping arr[low] and arr[mid]
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+
+                low++;
+                mid++;
+
+            } else if (arr[mid] == 1) {
+                mid++;
+
+            } else {
+                // swapping arr[mid] and arr[high]
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+
+                high--;
+            }
         }
-         int cnt1_end=cnt0+cnt1;
-
-        //Replace the places in the original array:
-        for (int i = 0; i < cnt0; i++) arr[i]=0;                // replacing 0's
-
-        for (int i = cnt0; i < cnt1_end; i++) arr[i]=1;       // replacing 1's
-
-        for (int i = cnt1_end; i < n; i++) arr[i]=2;          // replacing 2's
     }
 }
