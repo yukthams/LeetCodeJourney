7ce1cc5b05914b619131cac295c471c8faf1cc0b
yuktha_ms <166839897+yukthams@users.noreply.github.com>
2025-10-08 18:30:37 +0530

    Time: 9 ms (18.12%) | Memory: 78.2 MB (33.34%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,5 +1,41 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
+        ArrayList<Integer> pos = new ArrayList<>();
+        ArrayList<Integer> neg = new ArrayList<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                pos.add(nums[i]);
+            } else{
+                neg.add(nums[i]);
+            }
+        }
+        
+        // pos!=neg
+        if(pos.size()>neg.size()){
+            for(int i=0;i<neg.size();i++){
+                nums[i*2]=pos.get(i);
+                nums[(i*2)+1]=neg.get(i);
+            }
+            int indx=2*neg.size();
+            for(int i=neg.size();i<pos.size();i++){
+                nums[indx]=pos.get(i);
+                indx++;
+            }
+        } else{
+            for(int i=0;i<pos.size();i++){
+                nums[i*2]=pos.get(i);
+                nums[i*2+1]=neg.get(i);
+            }
+            int indx=2*pos.size();
+            for(int i=pos.size();i<neg.size();i++){
+                nums[indx]=neg.get(i);
+                indx++;
+            }
+        }
+        
+        return nums;
+        /*
         int n=nums.length;
         int[] ans=new int[n];
         int pos=0, neg=1;
@@ -13,5 +49,6 @@ class Solution {
             }
         }
         return ans;
+        */
     } 
 }
