f0cad50c1263230325c31ff296d2e4d25a48c2d1
yuktha_ms <166839897+yukthams@users.noreply.github.com>
2025-10-08 18:31:22 +0530

    Time: 3 ms (100.00%) | Memory: 77 MB (58.36%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,5 +1,20 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
+        int n=nums.length;
+        int[] ans=new int[n];
+        int pos=0, neg=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                 ans[pos]=nums[i];
+                 pos+=2;
+            } else{
+                ans[neg]=nums[i];
+                neg+=2;
+            }
+        }
+        return ans;
+
+        /* // pos!=neg
         ArrayList<Integer> pos = new ArrayList<>();
         ArrayList<Integer> neg = new ArrayList<>();
         int n=nums.length;
@@ -11,7 +26,6 @@ class Solution {
             }
         }
         
-        // pos!=neg
         if(pos.size()>neg.size()){
             for(int i=0;i<neg.size();i++){
                 nums[i*2]=pos.get(i);
@@ -35,20 +49,6 @@ class Solution {
         }
         
         return nums;
-        /*
-        int n=nums.length;
-        int[] ans=new int[n];
-        int pos=0, neg=1;
-        for(int i=0;i<n;i++){
-            if(nums[i]>0){
-                 ans[pos]=nums[i];
-                 pos+=2;
-            } else{
-                ans[neg]=nums[i];
-                neg+=2;
-            }
-        }
-        return ans;
         */
     } 
 }
